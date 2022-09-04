b5a24a20eebcad876e90152b720fce46641bc9e5
Aqib <aqibr777@gmail.com>
2022-09-04 20:12:58 +0500

    3-creating-and-listing-plans

---
+++ app/Http/Controllers/Subscriptions/PlanController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Subscriptions;
+
+use App\Plan;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PlanController extends Controller
+{
+    public function index()
+    {
+        $plans = Plan::get();
+
+        return view('subscriptions.plans', compact('plans'));
+    }
+}
