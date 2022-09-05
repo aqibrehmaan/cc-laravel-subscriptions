128b688907e33fe11a6275703cbbf82f9db9dc36
Aqib <aqibr777@gmail.com>
2022-09-05 21:54:55 +0500

    5-heading-to-the-checkout

---
+++ resources/views/subscriptions/checkout.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Checkout') }}</div>
+
+                <div class="card-body">
+                    <form action="" method="POST">
+                        <div class="form-group">
+                            <label for="name">Name on card</label>
+                            <input type="text" name="name" id="name" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="name">Card details</label>
+                            <div id="card-element"></div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary" id="card-button">
+                            Pay
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    const stripe = Stripe('{{ config('cashier.key') }}')
+    const elements = stripe.elements()
+    const cardElement = elements.create('card');
+
+    cardElement.mount('#card-element')
+
+</script>
+@endsection
+
