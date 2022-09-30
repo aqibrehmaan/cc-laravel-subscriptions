665a2bf3e4267bc1776316c4f8f6e78401316635
Aqib <aqibr777@gmail.com>
2022-09-30 22:16:46 +0500

    10-resume-subscriptions

---
+++ app/Http/Controllers/Account/Subscriptions/SubscriptionResumeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Account\Subscriptions;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubscriptionResumeController extends Controller
+{
+    //
+}
