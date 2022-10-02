65cc6e37fadabe23e24f3f9ba67fbc62bb7256ce
Aqib <aqibr777@gmail.com>
2022-10-02 18:36:11 +0500

    23-webhook-signing-secret-24-an-into-to-sca-25-handling-sca-at-the-checkout

--- app/Http/Controllers/Subscriptions/SubscriptionController.php
+++ app/Http/Controllers/Subscriptions/SubscriptionController.php
@@ -6,6 +6,7 @@ use App\Plan;
 use App\Rules\ValidCoupon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Laravel\Cashier\Exceptions\PaymentActionRequired;
 
 class SubscriptionController extends Controller
 {
@@ -36,9 +37,19 @@ class SubscriptionController extends Controller
         $plan = Plan::where('slug', $request->get('plan', 'monthly'))
                     ->first();
 
-        $request->user()->newSubscription('default', $plan->stripe_id)
-                ->withCoupon($request->coupon)
-                 ->create($request->token);
+        try {
+            $request->user()->newSubscription('default', $plan->stripe_id)
+            ->withCoupon($request->coupon)
+             ->create($request->token);
+
+        } catch(PaymentActionRequired $e) {
+            return redirect()->route(
+                'cashier.payment', [
+                $e->payment->id,
+                'redirect' => route('account.subscriptions')
+                ]);
+        }
+
 
         return back();
     }
